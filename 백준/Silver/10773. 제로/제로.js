5ccdad88e19dd5c9a9e9313f15d65e624051ae96
Kim To Kyun <58878190+ToKyun02@users.noreply.github.com>
2025-01-26 22:12:33 +0900

    [Silver IV] Title: 제로, Time: 180 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.js
+++ 백준/Silver/10773. 제로/제로.js
@@ -1,22 +1,43 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().split('\n').map(Number);
-
-
-function solution(){
-  let stack =[];
-  let result=0;
-  let length = input[0];
-  for(i=1; i<=length; i++){
-    if(input[i]===0){
-        stack.pop();
-    }
-    else{
-        stack.push(input[i]);
-    }
+class Stack {
+  constructor() {
+    this.array = [];
+    this.length = 0;
   }
-  for (let i = 0; i < stack.length; i += 1) {
-  result += stack[i];
+  push(value) {
+    this.array[this.length] = value;
+    this.length++;
+  }
+  pop() {
+    const value = this.array[this.length - 1];
+    this.length--;
+    this.array.length = this.length;
+    return value;
+  }
+  getArray() {
+    return this.array;
+  }
 }
+
+const solution = () => {
+  const input = require('fs')
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+  input.shift();
+
+  const stack = new Stack();
+
+  input.forEach((value) => {
+    if (value === 0) stack.pop();
+    else stack.push(value);
+  });
+  const result = stack.getArray().reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+
   console.log(result);
-}
+};
 
 solution();
